eed249a37fe8f6e7ae2b19261bb91d2adaf64f5d
LN Tripathy <lntripathyofficial@gmail.com>
2025-12-17 22:51:20 +0530

    second commit2

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState, useRef } from "react";
+
+import classNames from "classnames";
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+
+  // const handleNext = () => {
+  //     const nextIndex = (currentIndex + 1) % imageUrls.length;
+  //     setCurrentIndex(nextIndex);
+  // };
+
+  // const handlePrevious = () => {
+  //     const previousIndex =
+  //         (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+  //     setCurrentIndex(previousIndex);
+  // };
+
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
+    resetTimer();
+  };
+
+  // useEffect(() => {
+  //   const interval = setInterval(handleNext, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  useEffect(() => {
+    timerRef.current = setInterval(handleNext, 3000);
+
+    return () => clearInterval(timerRef.current);
+  }, []);
+
+  // TO RESET THE TIMER
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      {/* Image + arrows */}
+      <div className="flex items-center">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
+        />
+      </div>
+      {/* Dots */}
+      <div className="mt-2 flex items-center justify-center space-x-1">
+        {imageUrls.map((_, index) => (
+          <span
+            key={index}
+            className={classNames(
+              "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+              { "neeto-ui-bg-black": index === currentIndex }
+            )}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/Product.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Typography, Spinner } from "neetoui";
+import { isNotNil, append } from "ramda";
+
+import Carousel from "./Carousel";
+
+const Product = () => {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      );
+      setProduct(response.data);
+    } catch (error) {
+      console.log("An error occurred:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+  const totalDiscounts = mrp - offerPrice;
+  const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-6 pb-6">
+      <div>
+        <Typography className="py-2" style="h2" weight="semibold">
+          {name}
+        </Typography>
+        <hr className="border-2 border-black" />
+      </div>
+      <div className="mt-6 flex gap-4">
+        <div className="flex justify-center gap-16">
+          {isNotNil(imageUrls) ? (
+            <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+          ) : (
+            <img alt={name} className="w-48" src={imageUrl} />
+          )}
+        </div>
+        <div className="w-3/5 space-y-4">
+          <Typography>{description}</Typography>
+          <Typography>MRP: {mrp}</Typography>
+          <Typography className="font-semibold">
+            Offer price: {offerPrice}
+          </Typography>
+          <Typography className="font-semibold text-green-600">
+            {discountPercentage}% off
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;

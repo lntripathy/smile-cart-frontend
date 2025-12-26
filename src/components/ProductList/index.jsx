ebb65e7d6b76a65ec07ff840ab85c1cc56bcc405
LN Tripathy <lntripathyofficial@gmail.com>
2025-12-26 23:04:32 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,42 +4,55 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { NoData, Input } from "neetoui";
+import { Pagination, Input, NoData } from "neetoui";
 import { isEmpty } from "ramda";
+import { useTranslation } from "react-i18next";
+import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [searchKey, setSearchKey] = useState("");
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
+
+  const { t } = useTranslation();
+
   const debouncedSearchKey = useDebounce(searchKey);
 
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const productsParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productsParams);
 
   if (isLoading) {
     return <PageLoader />;
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="m-2">
-        <Header
-          shouldShowBackButton={false}
-          title="Smile cart"
-          actionBlock={
-            <Input
-              placeholder="Search products"
-              prefix={<Search />}
-              type="search"
-              value={searchKey}
-              onChange={event => setSearchKey(event.target.value)}
-            />
-          }
-        />
-      </div>
+    <div className="flex h-screen flex-col">
+      <Header
+        shouldShowBackButton={false}
+        title={t("title")}
+        actionBlock={
+          <Input
+            placeholder={t("searchProducts")}
+            prefix={<Search />}
+            type="search"
+            value={searchKey}
+            onChange={event => {
+              setSearchKey(event.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
+          />
+        }
+      />
       {isEmpty(products) ? (
-        <NoData className="h-full w-full" title="No products to show" />
+        <NoData className="h-full w-full" title={t("noData")} />
       ) : (
         <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
           {products.map(product => (
@@ -47,7 +60,16 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
-export default ProductList;
+
+export default withTitle(ProductList);

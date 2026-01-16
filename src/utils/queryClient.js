38fc75feae80140a89e63d2ee623d568b38348cf
LN Tripathy <lntripathyofficial@gmail.com>
2026-01-16 20:06:41 +0530

    Implemented caching of country and state data using persistQueryClient

--- src/utils/queryClient.js
+++ src/utils/queryClient.js
@@ -1,4 +1,8 @@
+import { QUERY_KEYS } from "constants/query";
+
 import { QueryClient, QueryCache } from "react-query";
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache(),
@@ -8,6 +12,20 @@ const queryClient = new QueryClient({
       staleTime: 3_600_000,
     },
   },
+});
+
+const localStoragePersistor = createWebStoragePersistor({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
+  maxAge: Infinity,
+  dehydrateOptions: ({ queryKey }) =>
+    [QUERY_KEYS.COUNTRIES, QUERY_KEYS.STATES].some(key =>
+      queryKey.includes(key)
+    ),
 });
 
 export default queryClient;
